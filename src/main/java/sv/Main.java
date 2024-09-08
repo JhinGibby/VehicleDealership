831dacdad6cfe66fe212d5d15014ea6b00f26dcd
Stoyan Dimitrov <stanley890x@gmail.com>
2024-09-08 04:05:28 +0300

    completion

--- src/main/java/sv/Main.java
+++ src/main/java/sv/Main.java
@@ -1,10 +1,7 @@
 package sv;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         Order order = new Order();
         order.initOrder();
